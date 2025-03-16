5843b4bb91e900dd9a0f2782fe5398cfaabfe28d
Fabian Faundez <fmauriciopoo@gmail.com>
2025-03-16 13:18:03 -0300

    listo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let amigos = [];
 let lista = document.getElementById('listaAmigos');
 document.getElementById("Sboton").disabled = false;
